0d19d1c572a76b90e5c4b77e00fec77e4247027a
Vaibhav Chaudhari <158283997+VAIBHAVCHAUDHARI1@users.noreply.github.com>
2024-02-04 11:32:09 +0530

    Create instance.tf

---
+++ aws/instance/instance.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# Create a VPC
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+# Create a subnet within the VPC
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a" 
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_instance" "ec2-instance" {
+  ami           = "ami-079db87dc4c10ac91"  
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet.id
+
+  tags = {
+    Name = "terraform-instance"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "vaibhav-terraform"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
