27ad995d6c63f12c73dcb813c3e3310c6fbce705
Vaibhav Chaudhari <158283997+VAIBHAVCHAUDHARI1@users.noreply.github.com>
2024-02-06 14:21:36 +0530

    Update pvc_dep_svc.tf

--- gcp/pvc_dep_svc.tf
+++ gcp/pvc_dep_svc.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.25.2"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path    = "~/.kube/config"
 }
@@ -24,17 +15,160 @@ resource "kubernetes_persistent_volume_claim" "example" {
         }
   }
 }
-resource "kubernetes_persistent_volume_claim" "example" {
+resource "kubernetes_deployment" "wp" {
   metadata {
-        name = "test-pvc"
+    name = "scalable-wp-example"
+    labels = {
+      App = "ScalablewpExample"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        App = "ScalablewpExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalablewpExample"
+        }
+      }
+      spec {
+        container {
+          image = "wordpress"
+          name  = "example"
+          env {
+            name  = "WORDPRESS_DB_HOST"
+            value = "dbexample" 
+          }
+          env {
+            name  = "WORDPRESS_DB_USER"
+            value = "root"
+          }
+          env {
+            name  = "WORDPRESS_DB_PASSWORD"
+            value = "centos"
+          }
+          env {
+            name  = "WORDPRESS_DB_NAME"
+            value = "unnati"
+          }
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_deployment" "db" {
+  metadata {
+    name = "scalable-db-example"
+    labels = {
+      App = "ScalabledbExample"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        App = "ScalabledbExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalabledbExample"
+        }
+      }
+      spec {
+        container {
+          image = "mysql:5.7"
+          name  = "example"
+        volume_mount {
+                name    = "test"
+                mount_path = "/data-store"
+        }
+
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = "centos"
+          }
+          env {
+            name  = "MYSQL_DATABASE"
+            value = "unnati"
+          }
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+  volume {
+        name = "test"
+        persistent_volume_claim {
+          claim_name = "test-pvc"
+        }
+        }
+}
+      }
+    }
+  }
+
+resource "kubernetes_service" "wp-service" {
+  metadata {
+    name = "wpxample"
   }
   spec {
-        storage_class_name = "standard-rwo"
-        access_modes = ["ReadWriteOnce"]
-        resources {
-        requests = {
-        storage = "5Gi"
-        }
-        }
+    selector = {
+      App = kubernetes_deployment.wp.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
+resource "kubernetes_service" "db-service" {
+  metadata {
+    name = "dbexample"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.db.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+
   }
 }
